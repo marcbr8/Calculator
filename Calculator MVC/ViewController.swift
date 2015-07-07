edf95a5c3cc4a1b2e97d3da3487fcd15f8b21964
Marc B <m.balaguerr@gmail.com>
2015-07-07 12:52:59 +0200

    Initial Commit

---
+++ Calculator MVC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator MVC
+//
+//  Created by Marc Balaguer on 07/07/15.
+//  Copyright (c) 2015 Marc Balaguer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
